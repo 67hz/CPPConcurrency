0e21b1acdd568adf529a5d6a70ec84987dfc33e2
Aaron Hinojosa <aaron@avenue6creative.com>
2019-03-13 13:10:29 -0500

    adding RAII mutex example

---
+++ mutexes/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <mutex>
+#include <list>
+#include <thread>
+#include <chrono>
+
+
+std::list<int> my_list;
+std::mutex m;
+
+void add_to_list(int const &x)
+{
+  // m.lock();
+  // RAII style auto lock/unlock
+  std::lock_guard<std::mutex> lg(m);
+  std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+  my_list.push_front(x);
+
+  // m.unlock();
+}
+
+void size()
+{
+  // m.lock();
+  std::lock_guard<std::mutex> lg(m);
+  int size = my_list.size();
+  // m.unlock();
+  std::cout << "Size of list is: " << size << std::endl;
+}
+
+std::list<int>* get_data()
+{
+  return &my_list;
+}
+
+
+
+int main()
+{
+  std::thread thread_1(add_to_list, 4);
+  std::thread thread_2(add_to_list, 11);
+  std::thread thread_3(size);
+
+  thread_1.join();
+  thread_2.join();
+  thread_3.join();
+
+  return 0;
+}
+
